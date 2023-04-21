7837ad831f5b9c94ab193581c4dfd4e54fa16ff6
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2023-04-21 13:32:52 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\LightsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::put("/lights/{status}", "LightsController@toggle");
-Route::get("/lights/status", "LightsController@getStatus");
+Route::put("/lights/{status}", [LightsController::class,"toggle"]);
+Route::get("/lights/status", [LightsController::class,"getStatus"]);
