d3fdb8c73d8a2eac19117dd8162cb2b49799081d
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2023-04-22 14:26:33 +0330

    Update Lights.php

--- app/Services/Lights.php
+++ app/Services/Lights.php
@@ -5,19 +5,19 @@ namespace App\Services;
 class Lights
 {
     public const CACHE_KEY = "lightsStatus";
-    
+
     public static function turnOn()
     {
-        cache()->set(self::CACHE_KEY, true);
+        cache()->put(self::CACHE_KEY, true);
     }
 
     public static function turnOff()
     {
-        cache()->set(self::CACHE_KEY, false);
+        cache()->put(self::CACHE_KEY, false);
     }
 
     public static function getStatus()
     {
-        cache()->get(self::CACHE_KEY, false);
+        return cache()->get(self::CACHE_KEY, false);
     }
 }
